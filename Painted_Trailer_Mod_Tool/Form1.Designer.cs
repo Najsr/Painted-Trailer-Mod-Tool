01e8501561122cfd4318835270e60f5d67ff301f
David <cesarovic@seznam.cz>
2017-04-28 22:18:34 +0200

    Added base

---
+++ Painted_Trailer_Mod_Tool/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Painted_Trailer_Mod_Tool
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxOldTrailers = new System.Windows.Forms.CheckBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.buttonPickColor = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxModName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkBoxOldTrailers
+            // 
+            this.checkBoxOldTrailers.AutoSize = true;
+            this.checkBoxOldTrailers.Location = new System.Drawing.Point(12, 43);
+            this.checkBoxOldTrailers.Name = "checkBoxOldTrailers";
+            this.checkBoxOldTrailers.Size = new System.Drawing.Size(150, 24);
+            this.checkBoxOldTrailers.TabIndex = 0;
+            this.checkBoxOldTrailers.Text = "Use old Trailers";
+            this.checkBoxOldTrailers.UseVisualStyleBackColor = true;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(155, 90);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 30);
+            this.buttonCreate.TabIndex = 1;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonPickColor
+            // 
+            this.buttonPickColor.Location = new System.Drawing.Point(12, 90);
+            this.buttonPickColor.Name = "buttonPickColor";
+            this.buttonPickColor.Size = new System.Drawing.Size(100, 30);
+            this.buttonPickColor.TabIndex = 2;
+            this.buttonPickColor.Text = "Pick Color";
+            this.buttonPickColor.UseVisualStyleBackColor = true;
+            this.buttonPickColor.Click += new System.EventHandler(this.buttonPickColor_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name of mod:";
+            // 
+            // textBoxModName
+            // 
+            this.textBoxModName.Location = new System.Drawing.Point(130, 10);
+            this.textBoxModName.Name = "textBoxModName";
+            this.textBoxModName.Size = new System.Drawing.Size(100, 27);
+            this.textBoxModName.TabIndex = 4;
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 168);
+            this.Controls.Add(this.textBoxModName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonPickColor);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.checkBoxOldTrailers);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Painted Trailer Mod Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxOldTrailers;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button buttonPickColor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxModName;
+    }
+}
+
---
+++ Painted_Trailer_Mod_Tool/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Media;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Painted_Trailer_Mod_Tool
+{
+    public partial class Form_Main : Form
+    {
+        string exeDir = AppDomain.CurrentDomain.BaseDirectory;
+
+        Color color;
+
+        string baseCompany = "company_permanent: company.permanent.<intname>\n{\n\tname: \"<intname>\"\n\tsort_name: \"<intname>\"\n\ttrailer_look: default\n}";
+
+        string[] companies = { "agronord.dlc_north", "aria_fd_albg.dlc_north", "aria_fd_esbj.dlc_north", "aria_fd_jnpg.dlc_north", "aria_fd_trbg.dlc_north", "batisse_hs", "bcp", "bhv.dlc_north", "bhv", "bjork.dlc_north", "cont_port.dlc_north", "drekkar.dlc_north", "euroacres", "eurogoodies", "fcp", "fle", "gnt.dlc_north", "ika_bohag.dlc_north", "ika_bohag", "itcc", "kaarfor", "konstnr.dlc_north", "konstnr_br.dlc_north", "konstnr_hs.dlc_north", "konstnr_wind.dlc_north", "lisette_log", "lkwlog", "marina.dlc_north", "ms_stein.dlc_north", "nbfc", "nord_crown.dlc_north", "nord_sten.dlc_north", "norrsken.dlc_north", "norr_food.dlc_north", "ns_chem.dlc_north", "ns_oil.dlc_north", "polarislines.dlc_north", "polar_fish.dlc_north", "posped", "quarry", "renar.dlc_north", "sag_tre.dlc_north", "sanbuilders", "scania_dlr", "scania_fac.dlc_north", "sellplan", "skoda", "stokes", "tradeaux", "trameri", "transinet", "tree_et", "vitas_pwr.dlc_north", "voitureux", "volvo_dlr", "volvo_fac.dlc_north", "vpc.dlc_north", "wgcc" };
+
+        string[] trailerNames = { "fridge", "krone_coolliner", "krone_profiliner", "schmitz_universal" };
+
+        string[] trailers = { "cement", "chemical_cistern", "food_cistern", "fuel_cistern", "livestock" };
+
+        string[] oldTrailers = { "reefer/chassis", "reefer/chassis_a", "container/chassis", "container/chassis" };
+
+        string[] newTrailers = { "krone/fridge", "krone/coolliner", "krone/profiliner", "schmitz/universal" };
+
+        string blankSii = Encoding.UTF8.GetString(Properties.Resources.emptytrailer);
+
+        string newSii;
+
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPickColor_Click(object sender, EventArgs e)
+        {
+            var result = colorDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+                color = colorDialog1.Color;
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            string paintName = textBoxModName.Text.Trim();
+            if (paintName.Equals(string.Empty) || color.IsEmpty)
+            {
+                MessageBox.Show("Name is empty or color hasn't been picked yet", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            Directory.CreateDirectory(exeDir + paintName + "/def/vehicle/trailer");
+            Directory.CreateDirectory(exeDir + paintName + "/def/company");
+
+            float colourR = color.R / 255f;
+            float colourG = color.G / 255f;
+            float colourB = color.B / 255f;
+
+            for (int i = 0; i < trailers.Length; i++)
+            {
+                newSii = blankSii;
+                newSii = newSii.Replace("<trailer>", trailers[i] + "/chassis");
+                if (trailers[i] == "chemical_cistern")
+                {
+                    newSii = newSii.Replace("<intname>", "chcistern");
+                }
+                else
+                {
+                    newSii = newSii.Replace("<intname>", trailers[i]);
+                }
+                newSii = newSii.Replace("<r>", colourR.ToString().Replace(",", "."));
+                newSii = newSii.Replace("<g>", colourG.ToString().Replace(",", "."));
+                newSii = newSii.Replace("<b>", colourB.ToString().Replace(",", "."));
+                using (StreamWriter siiout = new StreamWriter(exeDir + paintName + "/def/vehicle/trailer/" + trailers[i] + ".sii"))
+                {
+                    siiout.Write(newSii);
+                }
+                Console.Write(trailers[i] + " has been written\n");
+            }
+            if(checkBoxOldTrailers.Checked)
+            {
+                for(int i = 0; i < oldTrailers.Length; i++)
+                {
+                    newSii = blankSii;
+                    newSii = newSii.Replace("<trailer>", oldTrailers[i]);
+                    newSii = newSii.Replace("<intname>", trailerNames[i].Replace("_", "."));
+                    newSii = newSii.Replace("<r>", colourR.ToString().Replace(",", "."));
+                    newSii = newSii.Replace("<g>", colourG.ToString().Replace(",", "."));
+                    newSii = newSii.Replace("<b>", colourB.ToString().Replace(",", "."));
+                    using (StreamWriter siiout = new StreamWriter(exeDir + paintName + "/def/vehicle/trailer/" + trailerNames[i] + ".sii"))
+                    {
+                        siiout.Write(newSii);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < newTrailers.Length; i++)
+                {
+                    newSii = blankSii;
+                    newSii = newSii.Replace("<trailer>", newTrailers[i] + "/chassis");
+                    newSii = newSii.Replace("<intname>", trailerNames[i].Replace("_", "."));
+                    newSii = newSii.Replace("<r>", colourR.ToString().Replace(",", "."));
+                    newSii = newSii.Replace("<g>", colourG.ToString().Replace(",", "."));
+                    newSii = newSii.Replace("<b>", colourB.ToString().Replace(",", "."));
+                    using (StreamWriter siiout = new StreamWriter(exeDir + paintName + "/def/vehicle/trailer/" + trailerNames[i] + ".sii"))
+                    {
+                        siiout.Write(newSii);
+                    }
+                }
+            }
+            foreach (string currentCompany in companies)
+            {
+                using (StreamWriter siiout = new StreamWriter(exeDir + paintName + "/def/company/" + currentCompany + ".sii"))
+                {
+                    siiout.Write(baseCompany.Replace("<intname>", currentCompany.Replace(".dlc_north", "")));
+                }
+            }
+
+            SystemSounds.Beep.Play();
+        }
+    }
+}
